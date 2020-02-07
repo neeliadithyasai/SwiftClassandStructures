3074303c2ce18d745dacdf4d4a2789f06afd5ba3
adithyasai <neeliadithyasai@gmail.com>
2020-02-07 18:02:02 -0500

    Initial Commit

---
+++ SwiftClassandStructures/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftClassandStructures
+//
+//  Created by MacStudent on 2020-02-07.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
